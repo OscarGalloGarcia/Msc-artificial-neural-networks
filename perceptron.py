75755a99ae175d5ea609bc9853f541334bd6eae7
Oscar Gallo <oscar.gallo@inbest.cloud>
2026-09-06 19:09:43 -0600

    wip

--- perceptron.py
+++ perceptron.py
@@ -1,4 +1,17 @@
 import numpy as np 
 
-x = [[1, ]]
+#Datos de entrenamiento
+x_training = np.array([
+    [1, -0.5, 3, -2],
+    [1, 1, 1, 1]
+])
+
+#Datos para generalizar
+x_gen = 0
+
+#pesos
+weiths = np.array([-2.5,1.75])
+
+#factor de aprendizaje
+eta = 1/2 
 
