1a117d3946670a8659f31e1a234faa9d6bf2a12a
OscarGalloGarcia <oscar.gallo@inbest.cloud>
2026-09-16 16:57:39 -0600

    wip: parte 2

--- Pratica1/two-perceptrons.py
+++ Pratica1/two-perceptrons.py
@@ -1,6 +1,23 @@
 #Oscar Alberto Gallo Gacría 
 #Maestría en ciencias en Robótica e Inteligencia Artificial
-
+#Parte 2 
 #Clasificación de cuatro clases con dos perceptrones
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+X = np.array([
+    [1, 1, 1],
+    [1 ,1, 0],
+    [1 ,0 ,1],
+    [1 ,0 ,0],
+    [0 ,1 ,1],
+    [0 ,1 ,0],
+    [0 ,0 ,1],
+    [0 ,0 ,0],
+])
+#print(X.shape)
+
+# Deseado calculado manualmente
+d = np.array([1 ,1 ,1 ,0 ,1 ,0 ,0 ,0])
